f098fa87541c3f0cb20704e31b5979f9e02b75a1
Allison Shi <ams9451@nyu.edu>
2024-07-15 15:15:37 -0600

    code ILP from sean's linear programming slides

--- min_flip_ilp.py
+++ min_flip_ilp.py
@@ -1,7 +1,30 @@
 from gurobipy import *
 
 try:
-   pass
+   # Create a new model
+   m = Model("mip1")
+   # Create variables
+   x = m.addVar(vtype=GRB.BINARY, name="x")
+   y = m.addVar(vtype=GRB.BINARY, name="y")
+    
+   # Set objective
+   m.setObjective(5 * x + 8 * x, GRB.MAXIMIZE)
+
+   # Add constraint:
+   m.addConstr(x + y <= 6, "c0")
+   # Add constraint:
+   m.addConstr(5 * x + 9 * y <= 45, "c1")
+   # Add constraint:
+   m.addConstr(x >= 0, "c2")
+   m.addConstr(y >= 0, "c3")
+
+   m.update()
+   m.optimize()
+
+   for v in m.getVars():
+      print(v.varName, v.x)
+   
+   print('Optimal Objective function value:', m.objVal)
 
 except GurobiError:
     print('Error reported')
