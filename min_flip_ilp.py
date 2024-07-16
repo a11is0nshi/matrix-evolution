a9ec5a47194c908c1bb344f97e688bf47fbdc470
Allison Shi <ams9451@nyu.edu>
2024-07-16 11:54:34 -0600

    add colon

--- min_flip_ilp.py
+++ min_flip_ilp.py
@@ -38,9 +38,9 @@ try:
     B11 = np.zeros((n, n), dtype=object)
     for p in range(n):
         for q in range(p+1, n):
-          B01[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{i}, {j}, 0, 1]")
-          B10[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{i}, {j}, 1, 0]")
-          B11[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{i}, {j}, 1, 1]")
+            B01[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{i}, {j}, 0, 1]")
+            B10[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{i}, {j}, 1, 0]")
+            B11[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{i}, {j}, 1, 1]")
 
     # Add constraints to ensure that there is no conflict involving cols p and q. 
     count = 0
@@ -61,8 +61,8 @@ try:
     I = []
     J = []
    
-   for i in range (m) 
-    total = quicksum(D[i, j]*(1 - X[i, j]) + (1 - D[i, j])*(1 - X[i, j]) + (1 - D[i, j])*(X[i, j]) + D[i, j]*(X[i, j]) for i in I ) 
+    for i in range(m):
+        total = quicksum(D[i, j]*(1 - X[i, j]) + (1 - D[i, j])*(1 - X[i, j]) + (1 - D[i, j])*(X[i, j]) + D[i, j]*(X[i, j]) for i in I ) 
     
     model.setObjective(total, GRB.MINIMIZE)
     model.optimize()
