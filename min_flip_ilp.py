d9da0eea6273a9ac680d9e7076fe2616e54d66c0
robynburger <robynlburger@gmail.com>
2024-07-16 12:17:10 -0600

    edit to objective function min_flip_ilp

--- min_flip_ilp.py
+++ min_flip_ilp.py
@@ -38,9 +38,9 @@ try:
     B11 = np.zeros((n, n), dtype=object)
     for p in range(n):
         for q in range(p+1, n):
-            B01[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{i}, {j}, 0, 1]")
-            B10[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{i}, {j}, 1, 0]")
-            B11[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{i}, {j}, 1, 1]")
+            B01[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{p}, {q}, 0, 1]")
+            B10[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{p}, {q}, 1, 0]")
+            B11[p, q] = model.addVar(vtype=GRB.BINARY, name=f"B[{p}, {q}, 1, 1]")
 
     # Add constraints to ensure that there is no conflict involving cols p and q. 
     count = 0
@@ -58,16 +58,16 @@ try:
     
     # Set objective function - equal to num of bit flips?    
    
-    total = quicksum(quicksum(D[i, j]*(1 - X[i, j]) + (1 - D[i, j])*(1 - X[i, j]) + (1 - D[i, j])*(X[i, j]) + D[i, j]*(X[i, j]) for j in range(i+1, n)) for i in range(m))
+    total = quicksum(quicksum(D[i, j]*(1 - X[i, j]) + (1 - D[i, j])*(1 - X[i, j]) + (1 - D[i, j])*(X[i, j]) + D[i, j]*(X[i, j]) for j in range(n)) for i in range(m))
     
     model.setObjective(total, GRB.MINIMIZE)
     model.optimize()
     # Print results
-    for v in model.getVars():
-        print(v.varName, v.x)
-    print(model.getObjective())
+    #for v in model.getVars():
+       # print(v.varName, v.x)
+    
     print(D)
-    print('Optimal Objective function value:', model.objVal)
+    print('Optimal Objective function value:', model.getObjective())
 
 except GurobiError as ex:
     print('*********ERROR*********')
