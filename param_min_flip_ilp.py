5c5d24e2c1b5775c8d9e2d8a18d7444145f994e1
robynburger <robynlburger@gmail.com>
2024-07-19 14:25:13 -0600

    fixed M, N

--- param_min_flip_ilp.py
+++ param_min_flip_ilp.py
@@ -16,8 +16,8 @@ def getMatrix(name):
     return df.to_numpy()
 
 # given a Matrix M and an index u, getV(M, u) returns V = M \ M[u]
-def getV(M, u):
-    return np.delete(M, u, 0)
+def getV(D, u):
+    return np.delete(D, u, 0)
 
 
 def ILPincreased(u, V):
@@ -25,57 +25,58 @@ def ILPincreased(u, V):
     try:
         # D is input binary matrix
         # D = get_phyolin_matrix("Patient2_phyolin.csv")
-        m, n = V.shape[0], V.shape[1]
+        N = V.shape[0]
+        M = V.shape[1]
 
         m1 = Model("min_flip_model1")
         m2 = Model("min_flip_model2")
 
         
     # X is a conflict free matrix by constraints
-        X = m1.addMVar((m,n), vtype=GRB.BINARY, name="X")
-        Y = m2.addMVar((m,n), vtype=GRB.BINARY, name="Y")
+        X = m1.addMVar((N, M), vtype=GRB.BINARY, name="X")
+        Y = m2.addMVar((N, M), vtype=GRB.BINARY, name="Y")
 
 
 
 
         # Objective function
-        total = sum(sum(V[i, j]*(1 - X[i, j]) + (1 - V[i, j])*(X[i, j]) for j in range(n)) for i in range(m))
+        total = sum(sum(V[i, j]*(1 - X[i, j]) + (1 - V[i, j])*(X[i, j]) for j in range(M)) for i in range(N))
         m1.setObjective(total, GRB.MINIMIZE)
 
-        total2 = sum(sum(V[i, j]*(1 - Y[i, j]) + (1 - V[i, j])*(Y[i, j]) for j in range(n)) for i in range(m))
+        total2 = sum(sum(V[i, j]*(1 - Y[i, j]) + (1 - V[i, j])*(Y[i, j]) for j in range(M)) for i in range(N))
         m2.setObjective(total, GRB.MINIMIZE)
         
-        B01 = m1.addMVar((n,n), vtype=GRB.BINARY, name="B01")
-        B10 = m1.addMVar((n,n), vtype=GRB.BINARY, name="B10")
-        B11 = m1.addMVar((n,n), vtype=GRB.BINARY, name="B11")
+        B01 = m1.addMVar((M, M), vtype=GRB.BINARY, name="B01")
+        B10 = m1.addMVar((M,M), vtype=GRB.BINARY, name="B10")
+        B11 = m1.addMVar((M,M), vtype=GRB.BINARY, name="B11")
 
-        B012 = m1.addMVar((n,n), vtype=GRB.BINARY, name="B01")
-        B102 = m1.addMVar((n,n), vtype=GRB.BINARY, name="B10")
-        B112 = m1.addMVar((n,n), vtype=GRB.BINARY, name="B11")
+        B012 = m1.addMVar((M,M), vtype=GRB.BINARY, name="B01")
+        B102 = m1.addMVar((M,M), vtype=GRB.BINARY, name="B10")
+        B112 = m1.addMVar((M,M), vtype=GRB.BINARY, name="B11")
 
-        z = m2.addMVar((1, m), vtype =GRB.BINARY, name="z")
+        z = m2.addMVar((1, N), vtype =GRB.BINARY, name="z")
         u = m2.addVar(vtype=GRB.INTEGER, name="u")
         v = m2.addVar(vtype=GRB.INTEGER, name="v")
         V = m2.addVar(vtype=GRB.INTEGER, name="V")
 
         # Constraints (ensures no conflicts by checking each pair of columns (p, q)) 
-        m1.addConstrs(-1 * X[i, p] + X[i, q] <= B01[p,q] for p in range(n) for q in range(n) for i in range(m) if p != q)
-        m1.addConstrs(X[i, p] - X[i, q] <= B10[p,q] for p in range(n) for q in range(n) for i in range(m) if p != q)
-        m1.addConstrs(X[i, p] + X[i, q] - 1 <= B11[p,q] for p in range(n) for q in range(n) for i in range(m) if p != q)
-        m1.addConstrs(B01[p,q] + B10[p,q] + B11[p,q] <= 2 for p in range(n) for q in range(n) if p != q)
+        m1.addConstrs(-1 * X[i, p] + X[i, q] <= B01[p,q] for p in range(M) for q in range(M) for i in range(N) if p != q)
+        m1.addConstrs(X[i, p] - X[i, q] <= B10[p,q] for p in range(M) for q in range(M) for i in range(N) if p != q)
+        m1.addConstrs(X[i, p] + X[i, q] - 1 <= B11[p,q] for p in range(M) for q in range(M) for i in range(N) if p != q)
+        m1.addConstrs(B01[p,q] + B10[p,q] + B11[p,q] <= 2 for p in range(M) for q in range(M) if p != q)
 
-        m2.addConstrs(-1 * Y[i, p] + Y[i, q] <= B012[p,q] for p in range(n) for q in range(n) for i in range(m) if p != q)
-        m2.addConstrs(Y[i, p] - Y[i, q] <= B102[p,q] for p in range(n) for q in range(n) for i in range(m) if p != q)
-        m2.addConstrs(Y[i, p] + Y[i, q] - 1 <= B112[p,q] for p in range(n) for q in range(n) for i in range(m) if p != q)
-        m2.addConstrs(B012[p,q] + B102[p,q] + B112[p,q] <= 2 for p in range(n) for q in range(n) if p != q)
+        m2.addConstrs(-1 * Y[i, p] + Y[i, q] <= B012[p,q] for p in range(M) for q in range(M) for i in range(N) if p != q)
+        m2.addConstrs(Y[i, p] - Y[i, q] <= B102[p,q] for p in range(M) for q in range(M) for i in range(N) if p != q)
+        m2.addConstrs(Y[i, p] + Y[i, q] - 1 <= B112[p,q] for p in range(M) for q in range(M) for i in range(N) if p != q)
+        m2.addConstrs(B012[p,q] + B102[p,q] + B112[p,q] <= 2 for p in range(M) for q in range(M) if p != q)
     
     # Essential Partial Order Constraints
-        m2.addConstr(z[i] <= (Y[u, i] - Y[v, i] + 1)/2 for i in range(m))
-        m2.addConstr(sum(z[i] for i in range(m)) >= 1)
+        m2.addConstr(z[i] <= (Y[u, i] - Y[v, i] + 1)/2 for i in range(N))
+        m2.addConstr(sum(z[i] for i in range(N)) >= 1)
         m2.addConstr(0 <= u)
-        m2.addConstr(u < m)
+        m2.addConstr(u < N)
         m2.addConstr(0 <= v)
-        m2.addConstr(v < m)
+        m2.addConstr(v < N)
         m2.addConstr(v != u)
         m2.optimize()
         
@@ -110,11 +111,11 @@ def GetRelated(u, V):
             (Vl, Vr) = Split(V)
             return np.concatenate(GetRelated(u, Vl), GetRelated(u, Vr))
         
+def GetEssential(D, k):
+    pass
 
-M = getMatrix("nameoffile")
-Ru = np.empty(M.shape[0], dtype=object)
-for u in range(M.shape[0]):
-    V = getV(M, u)
-    Ru[u] = GetRelated(u, V)
-print(str(Ru))
+D = getMatrix("nameoffile")
+k = 10
+print(GetEssential)
+
 
