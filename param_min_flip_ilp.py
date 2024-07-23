f13c9744d1009e96ecb70914749d9122780389be
robynburger <robynlburger@gmail.com>
2024-07-23 10:40:43 -0600

    fix constr >= and make zero based

--- param_min_flip_ilp.py
+++ param_min_flip_ilp.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 
 # Change name and k to change file
-name = "small_test.csv"
+name = "smallest_test.csv"
 k = 2
 
 D =  pd.read_csv(name).to_numpy()
@@ -31,11 +31,10 @@ def ILP(u, Vset, prime):
         B10 = m.addMVar((M, M), vtype=GRB.BINARY, name="B10")
         B11 = m.addMVar((M, M), vtype=GRB.BINARY, name="B11")
 
-        # Objective function
         total = sum(sum((1 - D[i, j])*(X[i, j]) for j in range(M)) for i in range(N))
         m.setObjective(total, GRB.MINIMIZE)
 
-        # Ensure no conflicts by checking each pair of columns (p, q)) 
+        # Enforce no conflicts by checking each pair of columns (p, q)) 
         m.addConstrs(X[i, q] - X[i, p] <= B01[p,q] for p in range(M) for q in range(p+1, M) for i in range(N))
         m.addConstrs(X[i, p] - X[i, q] <= B10[p,q] for p in range(M) for q in range(p+1, M) for i in range(N))
         m.addConstrs(X[i, p] + X[i, q] <= B11[p,q] for p in range(M) for q in range(p+1, M) for i in range(N))
@@ -52,9 +51,9 @@ def ILP(u, Vset, prime):
             m.update()
             for i in range(M):
                 for v in range(Nv):
-                    m.addConstr(z[i, v] <= (X[u-1, i] - X[list(Vset)[v]-1, i] + 1)/2)
+                    m.addConstr(z[i, v] <= (X[u, i] - X[list(Vset)[v], i] + 1)/2)
             
-            m.addConstr(sum(z[i, v] for i in range(M) for v in range(Nv)) <= 1)
+            m.addConstr(sum(z[i, v] for i in range(M) for v in range(Nv)) >= 1)
             print(f"prime: {m.NumConstrs} constrs and {m.NumBinVars} vars")
 
         else:
@@ -96,18 +95,15 @@ def GetRelated(u, V):
         
 # Given a matrix D and positive integer k, GetEssential(D, k) outputs <e
 def GetEssential(D, k):
-    S = {num+1 for num in range(D.shape[0])}
+    S = {num for num in range(D.shape[0])}
     ess_set = set()
     R = []
     for u in S:
         V = S.difference({u})
         R.append(GetRelated(u, V))
-        P = {(u, y) for y in R[u-1]}
+        P = {(u, y) for y in R[u]}
         temp = ess_set.union(P)
         ess_set = temp
     return ess_set
 
-
 print(f"R: {GetEssential(D, k)}")
-
-
