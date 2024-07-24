b1aa487f92b9ac20d0bd774582f63280bb0ffddf
robynburger <robynlburger@gmail.com>
2024-07-24 14:34:18 -0600

    post_processing.py

--- post_processing.py
+++ post_processing.py
@@ -1,6 +1,9 @@
 # Given R, this will return <e
-import networkx as nx
+from networkx import *
 
-R = {(0,1), (0,2),(1,3),(1,4),(2,5),(2,6)}
+R = {(0,1),(0,1),(1,1),(0,2),(1,3),(1,4),(2,5),(2,6)}
 Rlist = list(R)
-G = nx.Graph(Rlist)
+G = DiGraph(Rlist)
+G.remove_edges_from(selfloop_edges(G)) # ex. (1,1)
+TR = transitive_reduction(G) # ex. (0,1), (1,2), (0,2)
+print(list(TR.edges))
